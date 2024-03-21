a0d72ac89307d9956fdcabc582a0c2bf3ab72cde
ChaeYi <chaeyi990731@gmail.com>
2024-03-21 22:36:57 +0900

    Fix: 스타일을 위한 클래스 추가

--- chaeyi-portfolio/src/app/page.tsx
+++ chaeyi-portfolio/src/app/page.tsx
@@ -1,15 +1,16 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import './globals.scss';
-import './page.module.scss';
+import './page.module.css';
 
 export default function Home() {
   return (
     <main>
       <div className="rootDiv">
+        <h1 className="title">풀스택 개발자 </h1>
         <div className="mainWidgets">
-          <Link href="/profile" style={{ textDecoration: 'none' }}>
-            <div className="maindiv"> Profile</div>
+          <Link href="/aboutme" style={{ textDecoration: 'none' }}>
+            <div className="maindiv"> About Me</div>
           </Link>
           <Link href="/project" style={{ textDecoration: 'none' }}>
             <div className="maindiv">Project</div>
