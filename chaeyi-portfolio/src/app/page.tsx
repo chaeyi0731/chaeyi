e2c56484d8a00460f28e750856d247d77d1d9ebb
ChaeYi <chaeyi990731@gmail.com>
2024-03-21 11:41:09 +0900

    Bad: 네비게이터 오류

--- chaeyi-portfolio/src/app/page.tsx
+++ chaeyi-portfolio/src/app/page.tsx
@@ -1,11 +1,17 @@
 import Image from 'next/image';
 import './globals.scss';
 import './page.module.css';
+import NavigationItem from '../component/NavigationItem';
 
 export default function Home() {
   return (
     <main>
-      <div></div>
+      <div>
+        <NavigationItem path="/profile">profile</NavigationItem>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </main>
   );
 }
