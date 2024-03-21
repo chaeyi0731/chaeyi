1419e691f42454c69e56101d5cd4d428060ee3fb
ChaeYi <chaeyi990731@gmail.com>
2024-03-21 13:02:59 +0900

    Fix: Link방식으로 제작 스타일은 클래스로 지정

--- chaeyi-portfolio/src/app/page.tsx
+++ chaeyi-portfolio/src/app/page.tsx
@@ -1,18 +1,26 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import './globals.scss';
-import './page.module.css';
+import './page.module.scss';
 
 export default function Home() {
   return (
     <main>
-      <div>
-        <Link href="/profile">
-          <div>profile</div>
-        </Link>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="rootDiv">
+        <div className="mainWidgets">
+          <Link href="/profile" style={{ textDecoration: 'none' }}>
+            <div className="maindiv"> Profile</div>
+          </Link>
+          <Link href="/project" style={{ textDecoration: 'none' }}>
+            <div className="maindiv">Project</div>
+          </Link>
+          <Link href="/stack" style={{ textDecoration: 'none' }}>
+            <div className="maindiv">Stack</div>
+          </Link>
+          <Link href="/contact" style={{ textDecoration: 'none' }}>
+            <div className="maindiv">Contact</div>
+          </Link>
+        </div>
       </div>
     </main>
   );
