605e949f15120f0fca5bf424bc213c5393830263
ChaeYi <chaeyi990731@gmail.com>
2024-03-21 23:34:59 +0900

    Fix: 간단한 html 구조
    내용은 학원가서 넣기

--- chaeyi-portfolio/src/app/aboutme/page.tsx
+++ chaeyi-portfolio/src/app/aboutme/page.tsx
@@ -1,8 +1,24 @@
 export default function AboutMe() {
   return (
     <main>
-      <div>
-        <h1>About Me</h1>
+      <div className="rootDiv">
+        <div>
+          <h1>About Me</h1>
+          <div className="aboutWidgets">
+            <div className="infowidgets">
+              <div className="profileImg"></div>
+              <div className="info">
+                <p></p>
+                <p></p>
+                <p></p>
+              </div>
+            </div>
+            <hr />
+            <div>
+              <p></p>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
