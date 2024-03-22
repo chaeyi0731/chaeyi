6bb877b73ab04bcf3b23cf2720066d7e278a511b
ChaeYi <chaeyi990731@gmail.com>
2024-03-22 11:09:11 +0900

    Fix: 내용 추가

--- chaeyi-portfolio/src/app/aboutme/page.tsx
+++ chaeyi-portfolio/src/app/aboutme/page.tsx
@@ -8,14 +8,21 @@ export default function AboutMe() {
             <div className="infowidgets">
               <div className="profileImg"></div>
               <div className="info">
-                <p></p>
-                <p></p>
-                <p></p>
+                <h1>👨‍💻 Web Developer</h1>
+                <ul>
+                  <li>
+                    안녕하세요! <span>꾸준히 성장하는 개발자</span> 이채이 입니다.
+                  </li>
+                  <li>소통과 배려를 좋아하기에 언제나 편안한 팀 분위기를 만들어 갈 수 있습니다</li>
+                  <li>항상 생글생글 웃으며 힘든 작업, 과정이어도 웃으며 작업 할 수 있습니다.</li>
+                  <li>무엇이든 관심이 생기면 빠르게 도전하고 실행합니다.</li>
+                  <li>단 한 줄의 코드라도 서비스의 가치를 담으려고 노력합니다.</li>
+                </ul>
               </div>
             </div>
             <hr />
             <div>
-              <p></p>
+              <p>dldldld</p>
             </div>
           </div>
         </div>
