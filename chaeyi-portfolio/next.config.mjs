612ce8128df0ebb9cfe901c2f9c09eafb5ce622c
ChaeYi <chaeyi990731@gmail.com>
2024-03-25 09:39:14 +0900

    Fix: 이미지 링크 권한 추가

--- chaeyi-portfolio/next.config.mjs
+++ chaeyi-portfolio/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['noticon-static.tammolo.com'],
+  },
+};
 
 export default nextConfig;
