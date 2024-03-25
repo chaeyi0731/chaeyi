b7f057a07367cc145c1e12f8064bfcd0a1d6a3f6
ChaeYi <chaeyi990731@gmail.com>
2024-03-25 11:59:52 +0900

    Package: 이미지최적화를 위한 이미지 주소 추가

--- chaeyi-portfolio/next.config.mjs
+++ chaeyi-portfolio/next.config.mjs
@@ -1,7 +1,8 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['noticon-static.tammolo.com'],
+    domains: ['stellatalk.s3.ap-northeast-2.amazonaws.com', 'noticon-static.tammolo.com'],
   },
 };
 
