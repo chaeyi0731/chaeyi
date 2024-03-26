ea3da79eef312a7040cd836a16631f5d0d29dc57
ChaeYi <chaeyi990731@gmail.com>
2024-03-26 17:16:59 +0900

    Fix: 설명 추가 및 디자인

--- chaeyi-portfolio/src/app/page.tsx
+++ chaeyi-portfolio/src/app/page.tsx
@@ -9,7 +9,8 @@ export default function Home() {
     <main>
       <motion.main initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 1 }}>
         <div className="rootDiv">
-          <h1 className="title">풀스택 개발자 </h1>
+          <p>소통과 배려를 좋아하는 사람 , 좋아하는 것을 탐구하는 사람 </p>
+          <h1 className="title">신입 개발자 이채이입니다🤗</h1>
           <div className="mainWidgets">
             <Link href="/aboutme" style={{ textDecoration: 'none' }}>
               <div className="maindiv"> About Me</div>
