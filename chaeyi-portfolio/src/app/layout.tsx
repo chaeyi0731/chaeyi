614ef01819b383dd5c6baf034a436877ce93db1d
ChaeYi <chaeyi990731@gmail.com>
2024-03-28 16:44:16 +0900

    Fix: 버전 표시

--- chaeyi-portfolio/src/app/layout.tsx
+++ chaeyi-portfolio/src/app/layout.tsx
@@ -17,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <p className="mainp"> version v1.0.1</p>
+      </body>
     </html>
   );
 }
